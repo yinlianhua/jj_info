fba7091a9082267eaf448f3a4b81fefd0ec2b449
ivan.yin <ivan.yin@ucloud.cn>
2025-03-31 18:51:38 +0800

    * fix

--- core/ihttp.js
+++ core/ihttp.js
@@ -5,46 +5,8 @@
 
 'use strict';
 
-let _           = require("underscore");
 let request     = require("request");
 let querystring = require("querystring");
-
-const keySort = (params) => {
-    let keys = _.keys(params);
-
-    let params_sort = {};
-
-    keys.sort();
-
-    _.each(keys, (key) => {
-        params_sort[key] = params[key];
-    });
-
-    return params_sort;
-}
-
-const sha1 = (str) => {
-    let crypto = require("crypto").createHash("sha1");
-
-    return crypto.update(str, 'utf8').digest('hex');
-}
-
-const verfyAC = (params, private_key) => {
-    let params_data = "";
-
-    delete params['Signature'];
-
-    params = keySort(params);
-
-    for (let key in params) {
-        params_data += key;
-        params_data += params[key];
-    }
-
-    params["Signature"] = sha1(params_data + private_key);
-
-    return params;
-}
 
 const get = (uri, timeout) => {
     return new Promise((resolve, reject) => {
@@ -92,14 +54,62 @@ const post = (uri, data, timeout) => {
     });
 };
 
-module.exports = async (params, use_post, timeout) => {
-    let url = global.INNER_API.url;
+const hget = (uri, timeout, header) => {
+    return new Promise((resolve, reject) => {
+        request({
+            uri     : uri,
+            method  : "GET",
+            timeout : timeout || 8000,
+            headers : header
+        }, function(error, response, body) {
+            if (error) {
+                resolve({
+                    "res" : error,
+                    "err" : true
+                });
+            } else {
+                resolve({
+                    "res" : JSON.parse(body),
+                    "err" : null
+                });
+            }
+        });
+    });
+};
 
-    let _params = verfyAC(_.extend({}, params, { PublicKey : global.INNER_API.public_key }), global.INNER_API.private_key);
+const hpost = (uri, data, timeout, header) => {
+    return new Promise((resolve, reject) => {
+        request({
+            uri     : uri,
+            method  : "POST",
+            json    : true,
+            body    : data,
+            timeout : timeout || 8000,
+            headers : header
+        }, function(error, response, body) {
+            if (error) {
+                resolve({
+                    "res" : error,
+                    "err" : true
+                });
+            } else {
+                resolve({
+                    "res" : body,
+                    "err" : null
+                });
+            }
+        });
+    });
+};
 
+module.exports = async (url, params, use_post, timeout=60000, header=false) => {
     if (!use_post) {
-        url += ("/?" + querystring.stringify(_params));
+        url += ("/?" + querystring.stringify(params));
     }
 
-    return use_post ? await post(url, _params, timeout) : await get(url, timeout);
+    if (header == false) {
+        return use_post ? await post(url, params, timeout) : await get(url, timeout);
+    }
+
+    return use_post ? await hpost(url, params, timeout, header) : await hget(url, timeout, header);
 };
